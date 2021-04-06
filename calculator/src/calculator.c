3daa07e01d7180eb31939ee778acc45d6e68a84a
Sonya Sheblova <sonyach.1110@gmail.com>
2021-04-06 11:41:08 +0300

    четвёртая версия

--- calculator/src/calculator.c
+++ calculator/src/calculator.c
@@ -1,19 +1,8 @@
 /*
  * Author: Sheblova Sonya
  *
- * Version 1:
- * available operations with numbers such as addition, subtraction,
- * multiplication, division, exponentiation and factorial of a number.
- *
- * Version 2:
- * added vector operations such as vector addition,
- * vector subtraction, and scalar product of vectors.
- * At the beginning of the program, you are prompted to select the program mode:
- * s:operations with numbers
- * v:operations with vectors
- *
- * Version 3:
- * work with files is implemented. Input format: sign, s or v, number of coordinates for vectors, data.
+ * Version 4 (with list)
+ * Input format: sign, s or v, number of coordinates for vectors, data.
  * example of input data for working with numbers: + s 2.3 -2
  * example of input data for working with vectors: - v 3 5 -2 0.1 9 105 0
  *
@@ -24,149 +13,229 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct list1 {
+	char sign, choose;
+	int size;
+	float *x, *y;
+	struct list1 *next;
+};
+
+struct list2 {
+	float *res;
+	struct list2 *res_next;
+};
+
+float* numb(char sign, float *x, float *y);
+float* vect(char sign, int size, float *a, float *b);
+float* add_numb(FILE *input, int size);
+void add_el(struct list1 *current, FILE *input);
+void res_add_el(struct list2 *res_current, struct list1 *current);
+
 int main(int argc, char *argv[]) {
-	float x, y, res;
-	float *a, *b;
-	int i, z, size;
-	long int f, S;
-	char n = 'y', choose, sign, in[100], out[100];
-	f = 1;
-	S = 1;
+	char n = 'y';
+	char in[20], out[20];
 	FILE *input, *output;
+	struct list1 *head, *current; //указатели на начало списка и текущий элемент
+	struct list2 *head_res, *current_res;
 	while (n == 'y') {
-		printf("\nEnter input file name: ");
+		printf("\nInput file name: ");
 		scanf("%s", in);
-		printf("Enter output file name: ");
+		printf("Output file name: ");
 		scanf("%s", out);
-
 		input = fopen(in, "r");
-		output = fopen(out, "w");
-		while (feof(input) == 0) {
-			fscanf(input, " %c", &sign);
-			fscanf(input, " %c", &choose);
-
-			switch (choose) {
-			case 's':
-				switch (sign) {
-				case '+':
-					fscanf(input, " %f %f", &x, &y);
-					fprintf(output, "%.2f + %.2f = %.2f", x, y, x + y);
-					break;
-				case '-':
-					fscanf(input, " %f %f", &x, &y);
-					fprintf(output, "%.2f - %.2f = %.2f", x, y, x - y);
-					break;
-				case '*':
-					fscanf(input, " %f %f", &x, &y);
-					fprintf(output, "%.2f * %.2f = %.2f", x, y, x * y);
-					break;
-				case '/':
-					fscanf(input, " %f %f", &x, &y);
-					if (y != 0)
-						fprintf(output, "%.2f / %.2f = %.2f", x, y, x / y);
-					else
-						fprintf(output, "error\n");
-					break;
-				case '!':
-					fscanf(input, "%d", &z);
-					if (z < 0) {
-						fprintf(output, "error");
-					} else {
-						if (z == 0) {
-							fprintf(output, "0");
-						} else {
-							for (i = 1; i <= z; i++) {
-								f *= i;
-							}
-							fprintf(output, "%d! = %ld", z, f);
-						}
-					}
-					break;
-				case '^':
-					fscanf(input, " %f %f", &x, &y);
-					for (i = 1; i <= y; i++) {
-						S *= x;
-					}
-					fprintf(output, "%.2f ^ %.2f = %.2ld", x, y, S);
-					break;
-				default:
-					fprintf(output, "Unknown operation");
-					break;
-				}
-				break;
-
-			case 'v':
-				fscanf(input, "%i", &size);
-				a = malloc(size * sizeof(float));
-				b = malloc(size * sizeof(float));
-				for (int i = 0; i < size; i++) {
-					fscanf(input, "%f", &a[i]);
-				}
-				for (int i = 0; i < size; i++)
-					fscanf(input, "%f", &b[i]);
-
-				switch (sign) {
-
-				case '+':
-					fprintf(output, "( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", a[i]);
-					fprintf(output, ") + ( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", b[i]);
-					fprintf(output, ") = ( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", a[i] + b[i]);
-					fprintf(output, ")");
-					break;
-
-				case '-':
-					fprintf(output, "( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", a[i]);
-					fprintf(output, ") - ( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", b[i]);
-					fprintf(output, ") = ( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", a[i] - b[i]);
-					fprintf(output, ")");
-					break;
-
-				case '*':
-					fprintf(output, "( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", a[i]);
-					fprintf(output, ") * ( ");
-					for (int i = 0; i < size; i++)
-						fprintf(output, "%.2f ", b[i]);
-					fprintf(output, ") = ");
-					for (int i = 0; i < size; i++)
-						res += a[i] * b[i];
-					fprintf(output, "%.2f ", res);
-					break;
-
-				default:
-					fprintf(output, "Unknown operation");
-				}
-				free(a);
-				free(b);
-				break;
-
-			default:
-				fprintf(output, "Unknown operation");
-				break;
+		if (feof(input) == 0) {
+			head = malloc(sizeof(struct list1)); // память для первого элемента списка
+			fscanf(input, " %c", &head->sign);
+			fscanf(input, " %c", &head->choose);
+			if (head->choose == 'v') {
+				fscanf(input, " %i", &head->size);
+			} else {
+				head->size = 1;
 			}
-			fprintf(output, "\n");
-		}
-		fclose(input);
-		fclose(output);
+			if (head->sign != '!') {
+				head->x = add_numb(input, head->size);
+				head->y = add_numb(input, head->size);
+			} else {
+				head->x = add_numb(input, head->size);
+				head->y = NULL;
+			}
+			current = head;
 
-		printf("\nDo you want to continue? (y/n)\n");
-		scanf("%s", &n);
-		if (n == 'n') {
-			printf("(^･ω･^)\n");
+			while (feof(input) == 0) { // добавление узлов списка, пока не закончится файл
+				add_el(current, input);
+				current = current->next;
+
+			}
+			head_res = malloc(sizeof(struct list2)); // память для списка для вывода
+			current = head;
+			if (current->choose == 'v') {
+				head_res->res = vect(current->sign, current->size, current->x,
+						current->y);
+			} else {
+				head_res->res = numb(current->sign, current->x, current->y);
+			}
+			head_res->res_next = NULL;
+			current = current->next;
+			current_res = head_res;
+			while (current != NULL) { // пока элемент не последниий
+				res_add_el(current_res, current);
+				// переустановка указателей на следующий элемент
+				current = current->next;
+				current_res = current_res->res_next;
+			}
+			current = head;
+			current_res = head_res;
+			fclose(input);
+			output = fopen(out, "w");
+			while (current != NULL) //запись ответа в output
+			{
+				if (current->choose == 'v') {
+					fprintf(output, "(");
+					for (int i = 0; i < current->size; i++) {
+						fprintf(output, " %.2f", current->x[i]);
+					}
+					fprintf(output, ") %c (", current->sign);
+					for (int i = 0; i < current->size; i++) {
+						fprintf(output, " %.2f", current->y[i]);
+					}
+					fprintf(output, " ) = ");
+					if (current->sign != '*') {
+						fprintf(output, "( ");
+						for (int i = 0; i < current->size; i++) {
+							fprintf(output, "%.2f ", current_res->res[i]);
+						}
+						fprintf(output, ")\n");
+					} else {
+						fprintf(output, "%.2f\n", current_res->res[0]);
+					}
+				} else if (current->choose == 's') {
+					fprintf(output, " %.2f %c %.2f = %.2f\n", current->x[0],
+							current->y[0], current->sign, current_res->res[0]);
+				}
+				current = current->next;
+				current_res = current_res->res_next;
+			}
+			fclose(output);
 		}
+		printf("do you want to continue? (y/n)");
+		scanf("%s", &n);
 	}
+	return 0;
 }
 
+float* numb(char sign, float *x, float *y) {
+	float f, S, *res_numb;
+	res_numb = malloc(sizeof(float));
+	switch (sign) {
+	case '+':
+		res_numb[0] = x[0] + y[0];
+		return res_numb;
+	case '-':
+		res_numb[0] = x[0] - y[0];
+		return res_numb;
+	case '*':
+		res_numb[0] = x[0] * y[0];
+		return res_numb;
+	case '/':
+		if (y != 0) {
+			res_numb[0] = x[0] / y[0];
+			return res_numb;
+		} else {
+			return 0;
+		}
+	case '!':
+		f = 1;
+		for (int i = 1; i <= x[0]; i++) {
+			f *= i;
+		}
+		res_numb[0] = f;
+		return res_numb;
+	case '^':
+		f = 1;
+		S = 1;
+		for (int i = 1; i <= y[0]; i++) {
+			S *= x[0];
+		}
+		res_numb[0] = S;
+		return res_numb;
+	}
+	return x;
+	return y;
+	free(x);
+	free(y);
+	free(res_numb);
+}
+
+float* vect(char sign, int size, float *a, float *b) {
+	float *res_vect;
+	switch (sign) {
+	case '+':
+		res_vect = malloc(size * sizeof(float));
+		for (int i = 0; i < size; i++) {
+			res_vect[i] = a[i] + b[i];
+		}
+		return res_vect;
+
+	case '-':
+		res_vect = malloc(size * sizeof(float));
+		for (int i = 0; i < size; i++) {
+			res_vect[i] = a[i] - b[i];
+		}
+		return res_vect;
+
+	case '*':
+		res_vect = malloc(sizeof(float));
+		res_vect[0] = 0;
+		for (int i = 0; i < size; i++) {
+			res_vect[0] += a[i] * b[i];
+		}
+		return res_vect;
+	}
+	return a;
+	return b;
+	free(a);
+	free(b);
+	free(res_vect);
+}
+
+// считывание указателей
+float* add_numb(FILE *input, int size) {
+	float *numb;
+	numb = malloc(size * sizeof(float));
+	for (int i = 0; i < size; i++) {
+		fscanf(input, "%f", &numb[i]);
+	}
+	return numb;
+}
+
+// добавление элемента списка
+void add_el(struct list1 *current, FILE *input) {
+	struct list1 *z = malloc(sizeof(struct list1));
+	fscanf(input, " %c", &z->sign);
+	fscanf(input, " %c", &z->choose);
+	if (z->choose == 'v') {
+		fscanf(input, " %i", &z->size);
+	} else {
+		z->size = 1;
+	}
+	if (z->sign != '!') {
+		z->x = add_numb(input, z->size);
+		z->y = add_numb(input, z->size);
+	} else {
+		z->x = add_numb(input, z->size);
+		z->y = NULL;
+	}
+	z->next = NULL; // последний элемент списка
+	current->next = z; // установка указателя
+}
+
+void res_add_el(struct list2 *res_current, struct list1 *current) {
+	struct list2 *z_res = malloc(sizeof(struct list1));
+	if (current->choose == 'v') {
+		z_res->res = vect(current->sign, current->size, current->x, current->y);
+	} else {
+		z_res->res = numb(current->sign, current->x, current->y);
+	}
+	z_res->res_next = NULL;
+	res_current->res_next = z_res;
+}
