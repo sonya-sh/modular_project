dfc04b83079895d51fe81cb536e531a960ce91f6
Sonya Sheblova <sonyach.1110@gmail.com>
2021-06-10 12:22:40 +0300

    Шестая версия (queue)

--- calculator/src/calculator.c
+++ calculator/src/calculator.c
@@ -1,125 +1,237 @@
 /*
-
- 5 версия(стек)
- Использование стека для обратной польской нотации.
- Пример ввода входных данных: 2 1 + 7 * 2 - n (в конце каждого выражения прописать "n")
- Строка с промежуточными и итоговым результатами: 3.00  21.00  19.00
-
+ * Использование очереди для выполнения группы заданий
+ * Пример выходных данных:
+ * + c 5 6
+ * - v 3 9 8 7 1 2 3
+ * Поддерживается работа только с положительными числами типа int :(
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct element {
-	float data;
-	struct element *next;
-} stack;
+// структура для элемента очереди
+typedef struct data {
+	char *elem;
+	struct data *next;
+} data;
 
-stack *head_stack = NULL;
+// структура для очереди
+typedef struct queue_t {
+	data *head;
+	data *tail;
+} queue_t;
 
-void push_element(stack **head, float data);
-float pop_element(stack **head);
-float numb(char sign, float x, float y);
+// инициализация пустой очереди
+queue_t init_queue() {
+	queue_t queue; // создаём очередь
+	queue.head = NULL;
+	queue.tail = NULL;
+	return queue;
+}
 
-int main(int argc, char *argv[]) {
-	char n = 'y', str[20], t = '0';
+void enqueue(queue_t *queue, char *value);
+char* dequeue(queue_t *queue);
+int is_empty(queue_t queue);
+char* read_string(FILE *f1);
+int is_digit(char c);
+int read_int(char *s, int *i);
+double factorial(int f);
+
+void numbers(FILE *output, char *s, char sign);
+void vectors(FILE *output, char *s, char sign);
+void calc(FILE *input, char *s);
+
+int main() {
+	char n = 'y';
 	char in[20], out[20];
-	int i = 0;
-	float num, x, y;
 	FILE *input, *output;
 	while (n == 'y') {
-		printf("Enter name of input file:");
-		scanf(" %s", in);
-		printf("Enter name of output file:");
-		scanf(" %s", out);
+		printf("Enter name to input file: ");
+		scanf("%s", in);
 		input = fopen(in, "r");
-		output = fopen(out, "w");
-		while (feof(input) == 0) {
-			fscanf(input, "%c", &str[i]);
-			if (str[i] == ' ') {
-				if (t == '0') {
-					num = strtof(str, NULL); // разделяет строку на отдельные элементы
-					push_element(&head_stack, num);
-					i = 0;
+		queue_t queue = init_queue(); // создаём пустую очередь
 
-				}
-			} else if (str[i] == '*' || str[i] == '+' || str[i] == '-'
-					|| str[i] == '/' || str[i] == '^') {
-				y = pop_element(&head_stack);
-				x = pop_element(&head_stack);
-				push_element(&head_stack, numb(str[i], x, y));
-				fprintf(output, "%.2f  ", numb(str[i], x, y));
-				t = '1';
-			} else if (str[i] == 'n') {
-				while (head_stack != NULL)
-					pop_element(&head_stack);
-				fprintf(output, "\n");
-			} else {
-				t = '0';
-				i = i + 1;
-			}
-		}
+		while (!feof(input))
+			enqueue(&queue, read_string(input)); // добавляем в очередь строки
 
 		fclose(input);
+
+		printf("Enter name to output file: ");
+		scanf("%s", out);
+
+		output = fopen(out, "w");
+
+		while (!is_empty(queue)) { // пока не кончится очередь
+			calc(output, dequeue(&queue)); // вычисляем выражение
+		}
 		fclose(output);
-		printf("Do you want to continue? (y/n)");
-		scanf(" %c", &n);
+		printf("do you want to continue? (y/n)");
+		scanf("%s", &n);
 	}
 	return 0;
 }
 
-float numb(char sign, float x, float y) {
-	float res, f, S;
-	switch (sign) {
-	case '+':
-		res = x + y;
-		return res;
-		break;
-	case '-':
-		res = x - y;
-		return res;
-		break;
-	case '*':
-		res = x * y;
-		return res;
-		break;
-	case '/':
-		if (y != 0)
-			res = x / y;
-		return res;
-		break;
-	case '!':
-		f = 1;
-		for (int i = 1; i <= x; i++) {
-			f *= i;
-		}
-		res = f;
-		return res;
-	case '^':
-		f = 1;
-		S = 1;
+// добавление элемента в конец очереди
+void enqueue(queue_t *queue, char *value) {
+	data *node = (data*) malloc(sizeof(data));
+	node->elem = value;
+
+	node->next = NULL;
+	if (queue->tail == NULL) {
+		queue->head = node;
+	} else {
+		queue->tail->next = node;
+	}
+	queue->tail = node;
+}
+
+// удаление элемента из очереди
+char* dequeue(queue_t *queue) {
+	char *elem = queue->head->elem;
+
+	data *tmp = queue->head;
+	queue->head = queue->head->next;
+	free(tmp);
+	if (queue->head == NULL) {
+		queue->tail = NULL;
+	}
+
+	return elem;
+}
+
+// проверка очереди на пустоту
+int is_empty(queue_t queue) {
+	return queue.head == NULL;
+}
+
+// считывание строки из файла
+char* read_string(FILE *input) {
+	int size = 0; // размер строки
+	int len = 1; // ёмкость строки
+	char *s = (char*) malloc(len * sizeof(char));
+	for (char c = fgetc(input); !feof(input) && c != '\n'; c = fgetc(input)) {
+		s[size++] = c;
+
+	}
+
+	s[size] = '\0';
+
+	return s;
+}
+
+// проверка на цифру
+int is_digit(char c) {
+	return (c >= '0' && c <= '9');
+}
+
+// ввод числа из строки
+int read_int(char *s, int *i) {
+	int value = 0;
+
+	while (s[*i] == ' ')
+		(*i)++;
+
+	while (s[*i] && is_digit(s[*i]))
+		value = value * 10 + s[(*i)++] - '0';
+
+	return value;
+}
+
+
+double factorial(int f) {
+	double fact = 1;
+
+	for (int i = 2; i <= f; i++)
+		fact *= i;
+
+	return fact;
+}
+
+void numbers(FILE *output, char *s, char sign) {
+	int index = 3;
+	int x = read_int(s, &index);
+
+	if (sign == '!') {
+		fprintf(output, "%.0lf\n", factorial(x));
+		return;
+	}
+
+	int y = read_int(s, &index);
+
+	if (sign == '+') {
+		fprintf(output, "%d\n", x + y);
+	} else if (sign == '-') {
+		fprintf(output, "%d\n", x - y);
+	} else if (sign == '*') {
+		fprintf(output, "%d\n", x * y);
+	} else if (sign == '/') {
+		if (y == 0)
+			fprintf(output, "division by zero\n");
+		else
+			fprintf(output, "%lf\n", (double) x / y);
+	} else if (sign == '^') {
+
+		int S = 1;
 		for (int i = 1; i <= y; i++) {
 			S *= x;
 		}
-		res = S;
-		return res;
+		fprintf(output, "%d\n", S);
 	}
-	return x;
-	return y;
+
+	else {
+		fprintf(output, "unknown operation '%c'\n", sign);
+	}
 }
 
-void push_element(stack **head, float data) {
-	stack *z = malloc(sizeof(stack));
-	z->next = *head;
-	z->data = data;
-	*head = z;
+void vectors(FILE *output, char *s, char sign) {
+	int index = 3;
+	int n = read_int(s, &index); // размерность вектора
+
+	int *v1 = (int*) malloc(n * sizeof(int));
+	int *v2 = (int*) malloc(n * sizeof(int));
+
+	// считываем вектора
+	for (int i = 0; i < n; i++)
+		v1[i] = read_int(s, &index);
+
+	for (int i = 0; i < n; i++)
+		v2[i] = read_int(s, &index);
+
+	if (sign == '*') {
+		int res = 0;
+
+		for (int i = 0; i < n; i++)
+			res += v1[i] * v2[i];
+
+		fprintf(output, "%d\n", res);
+	} else if (sign == '+' || sign == '-') {
+		if (sign == '+') {
+			for (int i = 0; i < n; i++)
+				fprintf(output, "%d ", v1[i] + v2[i]);
+			fprintf(output, "\n");
+		} else {
+			for (int i = 0; i < n; i++)
+				fprintf(output, "%d ", v1[i] - v2[i]);
+			fprintf(output, "\n");
+		}
+
+	} else {
+		fprintf(output, "unknown operation '%c'\n", sign);
+	}
+
+	free(v1);
+	free(v2);
 }
 
-float pop_element(stack **head) {
-	stack *z;
-	float data;
-	z = *head;
-	*head = (*head)->next;
-	data = z->data;
-	return data;
+// вычисление значения выражения
+void calc(FILE *input, char *s) {
+	char sign = s[0];
+	char op = s[2];
+
+	if (op == 'c') {
+		numbers(input, s, sign);
+	} else if (op == 'v') {
+		vectors(input, s, sign);
+	}
 }
 
